c4dae426572b08ddd09b1a5bdf7b7f32bcfb24cd
ephekt <mfrosengarten@gmail.com>
2012-03-07 17:36:33 -0800

    adding in okcupid class and massive refactor

---
+++ lib/ok_cupid.rb
@@ -0,0 +1,78 @@
+require 'base64'
+
+class OkCupid
+  def initialize username = "i_like_lamps", password = Base64.decode64("anVua3lhcmQxOTg3Ng==\n")
+    @agent = Mechanize.new
+    @agent.user_agent_alias = "Mac Safari"
+    @username = username
+    @password = password
+  end
+
+  def login
+    page = @agent.get "http://www.okcupid.com/"
+
+    if page.body.include? 'name="loginf"'
+      # login
+      form = page.form_with :name => 'loginf'
+      form.username = @username
+      form.password = @password
+      form.submit
+    end
+
+    page = @agent.get "http://www.okcupid.com/"
+        
+    !page.body.include? 'name="loginf"'
+  end
+  
+  def get_url url = nil
+    return false unless url
+    
+    @agent.get url
+  end
+  
+  # usr-([_A-Za-z0-9]+)\\\".*?location\\\">([A-Za-z-_, ]+)<\/p>
+  def match_usernames max_profiles = 2000, step = 500
+    usernames = []
+    
+    low = 1
+    (max_profiles/step).times do
+      p = get_url "https://www.okcupid.com/match?timekey=1&matchOrderBy=SPECIAL_BLEND&use_prefs=1&discard_prefs=1&low=#{low}&count=#{STEP}&ajax_load=1"
+      p.body.scan(/usr-([_A-Za-z0-9]+)\\\"/).each do |username_array|
+        usernames << username_array.first
+      end
+      
+      low += step      
+    end
+    
+    usernames.uniq
+  end
+  
+  def profile_for username = nil
+    return false unless username
+    
+    profile = get_url "http://m.okcupid.com/profile/#{username}"
+    aso = profile.search("//p[@class='aso']").inner_text.split(/\s\/\s/)
+    location= profile.search("//p[@class='location']").inner_text
+    
+    {
+      :age => aso[0],
+      :sex => aso[1],
+      :orientation => aso[2],
+      :status => aso[3],
+      :location => location
+    }
+  end
+    
+  def profile_pics_for username = nil
+    return false unless username
+    profile = get_url "https://www.okcupid.com/profile/#{username}/photos"
+    
+    pic_thumbs = profile.search("//div[@id='profile_thumbs']//img/@src").map(&:value)
+    pic_fulls = profile.search("//div[@class='img']//img/@src").map(&:value)    
+    
+    {
+      :small => pic_thumbs,
+      :big => pic_fulls
+    }
+  end
+end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,114 +1,137 @@
 require "rubygems"
-require 'base64'
 require 'bundler'
 
 Bundler.require
 
-@agent = Mechanize.new
-@agent.user_agent_alias = "Mac Safari"
-
-page = @agent.get "http://www.okcupid.com/"
-
-if page.body.include? "sidebar_signin_password"
-  # login
-  form = page.form_with :name => 'loginf'
-  form.username = "i_like_lamps"
-  form.password = Base64.decode64("anVua3lhcmQxOTg3Ng==\n")
-  form.submit
-end
+# Classes
+require './ok_cupid.rb'
 
 # Database Login
-DB = SQLite3::Database.new( "okcupid.db" )
+DB = SQLite3::Database.new( "../okcupid.db" )
 
-DB.execute("CREATE TABLE IF NOT EXISTS `profiles` (
-    `username` varchar(128) PRIMARY KEY,
-    `last_fetch_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  )")
+DB.execute("CREATE TABLE IF NOT EXISTS profiles (username varchar(128)  NOT NULL  PRIMARY KEY,`last_fetch_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,location Varchar(128) DEFAULT NULL,sex Varchar(16),age INTEGER,orientation Varchar(64),status Varchar(64))")
+DB.execute("CREATE TABLE IF NOT EXISTS pictures (username varchar(128)  NOT NULL,size varchar(32) NOT NULL,url varchar(256) NOT NULL)")
 
 filters = [
   "JOIN",
   "SPECIAL_BLEND"
 ]
 
+@ok = OkCupid.new
+
+raise "Unable to continue, login failed" unless @ok.login
+
 MAX_PROFILES = 2000
 STEP = 500
 def fetch_usernames
-  low = 1
-  dupitydup = 0
-  newpitynewps = 0
-
-  (MAX_PROFILES/STEP).times do
-    p = @agent.get "https://www.okcupid.com/match?timekey=1&matchOrderBy=SPECIAL_BLEND&use_prefs=1&discard_prefs=1&low=#{low}&count=#{STEP}&ajax_load=1"
-
-    usernames = p.body.scan(/usr-([_A-Za-z0-9]+)\\\"/i).each do |username|
-      begin
-        # call first on uid as nokogiri gives us an array with
-        # the uid as the only element
-        q = "INSERT INTO `profiles` (username) VALUES ('#{username.first}')"
-        DB.execute q
-        newpitynewps += 1
-      rescue Exception => e
-        if e.to_s.include? "is not unique"
-          dupitydup += 1
-        else
-          puts "#{q} FAILED: #{e}"
-        end
+  newbs = 0
+  dups = 0
+  @ok.match_usernames(MAX_PROFILES, STEP).each do |username|
+    begin
+      q = "INSERT INTO `profiles` (username) VALUES ('#{username}')"
+      DB.execute q
+      newbs += 1
+    rescue Exception => e
+      if e.to_s.include? "not unique"
+        dups += 1
+      else
+        puts "#{q} FAILED: #{e}"
       end
     end
-    low += STEP
   end
-
-  puts "#{dupitydup} dupitydups"
-  puts "#{newpitynewps} newpitynewps"
+  
+  puts "Added #{newbs} items and found #{dups} duplicates"
 end
+
 
 def fetch_profile_pics
-=begin
-  * Skip a user if we find a picture of them
-  ** Eventually, we will want to do an update on current users we already have
-  ** images stored for. But for now let's focus on new users only
-=end
-
-  puts "Fetching #{STEP} items at a time..."
-  hydra = Typhoeus::Hydra.new(:max_concurrency => 15) # keep from killing some servers
-
-  begin
-    usernames = DB.execute("SELECT username FROM `profiles`").map &:pop
-
-    usernames.each do |username|
-      if Dir.glob("profile_pictures/#{username}_*").any?
-        puts "SKIP: #{username}"
-        next
-      else
-        puts "FETCH: #{username}"
-      end
-
-      profile = @agent.get "https://www.okcupid.com/profile/#{username}/photos"
-      profile.search("//div[@id='profile_thumbs']//img/@src").each do |img_tag|
-        filename = "#{username}_#{img_tag.value.split('/').last}"
-        stored_filename = "profile_pictures/#{filename}"
-
-        next if File.exists? stored_filename
-
-        request = Typhoeus::Request.new(img_tag.value)
-        request.on_complete do |response|
-          File.open(stored_filename,"w+") do |f|
-            f.write response.body
-          end
+  # Find Users who we do not have any pictures for yet.
+  # We can do a refresh some other time.
+  q = "
+    SELECT username FROM `profiles` 
+    WHERE username NOT IN (SELECT DISTINCT username from `pictures`)
+  "
+  usernames = DB.execute(q).map(&:pop)
+  puts "Fetching #{usernames.size} profiles' pictures... (estimated: #{usernames.size*3} pictures)"
+  usernames.each do |username|
+    profile = @ok.profile_pics_for username
+    
+    profile.each do |size,images|
+      images.each do |url|
+        begin
+          q = "INSERT INTO `pictures` (username,url,size) 
+                VALUES ('#{username}','#{url}','#{size}')"
+          DB.execute q
+        rescue Exception => e
+          puts "Failed to #{q}; #{e}"
         end
+      end
+    end
+  end
+end
 
-        hydra.queue request
+def update_profile_details
+  q = "SELECT username FROM `profiles` WHERE location IS NULL OR location = ''"
+  usernames = DB.execute(q).map(&:pop)
+  puts "Updating #{usernames.size} profiles"
+  usernames.each do |username|
+    p = @ok.profile_for username
+    begin
+      qr = "REPLACE INTO profiles (username,sex,age,orientation,status,location)
+            VALUES ('#{username}',
+                    '#{p[:sex]}',
+                    '#{p[:age]}',
+                    '#{p[:orientation]}',
+                    '#{p[:status]}',
+                    '#{p[:location]}'
+            )"
+      DB.execute qr
+    rescue Exception => e
+      puts "Failed to #{qr}; #{e}"
+    end
+  end
+end
+
+
+def download_pictures
+  hydra = Typhoeus::Hydra.new(:max_concurrency => 10) # keep from killing some servers
+  
+  q = "SELECT username,size,url FROM `pictures` LIMIT 40"
+  pictures = DB.execute(q)
+  puts "#{pictures.size} to process"
+  
+  pictures.each do |picture|
+    username = picture[0]
+    size = picture[1]
+    url = picture[2]
+
+    filename = "#{username}_#{url.split('/').last}"
+    download_directory = "../public/profile_pictures/#{size}/"
+    if File.exists? download_directory + filename
+      puts "SKIP: #{filename}"
+      next
+    else
+      puts "FETCH: #{filename}"
+    end
+
+    request = Typhoeus::Request.new(url)
+    request.on_complete do |response|
+      File.open(download_directory + filename,"w+") do |f|
+        f.write response.body
       end
     end
 
-  ensure
-    hydra.run
+    hydra.queue request
   end
+  
+  hydra.run
 end
+
 
 case ARGV[0]
   when "usernames" then fetch_usernames
   when "pictures" then fetch_profile_pics
-  when "both" then fetch_usernames; fetch_profile_pics
-  else puts "Usage: ruby scraper.rb [usernames|pictures|both]"
+  when "profile_data" then update_profile_details
+  when "download_pictures" then download_pictures    
+  else puts "Usage: ruby scraper.rb [usernames|pictures|profile_data|download_pictures]"
 end
