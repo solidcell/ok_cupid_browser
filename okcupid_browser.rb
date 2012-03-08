44132edd81e4b868cfc69033d85377f80e553e1b
Jesse Farless <solidcell@gmail.com>
2012-03-08 15:14:09 -0800

    not using the test array for the template anymore

--- okcupid_browser.rb
+++ okcupid_browser.rb
@@ -2,7 +2,7 @@ class OKCBrowser < Sinatra::Base
   set :erb, :format => :html5
 
   get "/" do
-    q = "SELECT username, url FROM pictures WHERE size = 'small' GROUP BY username"
+    q = "SELECT username, url FROM pictures WHERE size = 'small' GROUP BY username LIMIT 10"
     db = SQLite3::Database.new "db/okcupid.db"
     @users = db.execute(q)
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,7 @@
 $(document).ready(function() {
   var image_paths = $("#image_paths").html();
   image_paths = $.parseJSON(image_paths);
-  test_image_paths = [
-    {'url':'profile_pictures/small/chottochagol_13533430516552857947.jpeg',
-     'username':'chottochagol'},
-    {'url':'profile_pictures/small/Cierrautumn_8193661997457597102.jpeg',
-     'username':'Cierrautumn'}
-  ]
-
-  Tempo.prepare("pictures").render(test_image_paths);
+  Tempo.prepare("pictures").render(image_paths);
 
   $("img.lazy").lazyload();
 });
