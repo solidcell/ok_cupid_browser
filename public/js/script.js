b7b97a621f803c2b0b8db880803c752ee287a0fc
Jesse Farless <solidcell@gmail.com>
2012-03-08 14:52:59 -0800

    using JSON and Tempo for templating

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
+  var image_paths = $("#image_paths").html();
+  image_paths = $.parseJSON(image_paths);
+  test_image_paths = [
+    {'url':'profile_pictures/small/chottochagol_13533430516552857947.jpeg',
+     'username':'chottochagol'},
+    {'url':'profile_pictures/small/Cierrautumn_8193661997457597102.jpeg',
+     'username':'Cierrautumn'}
+  ]
+
+  Tempo.prepare("pictures").render(test_image_paths);
+
   $("img.lazy").lazyload();
 });
