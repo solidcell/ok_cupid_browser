f385b491e3815e21956ad70b52ded41e356d270b
Jesse Farless <solidcell@gmail.com>
2012-03-07 18:17:15 -0800

    working on endless ajax scroll

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,23 @@
-/* Author:
-
-*/
-
 $(document).ready(function() {
-	$('h1.linkable').click( function() {
-		$("#"+this.id+"_data").toggle();
-	});
+  $('ul').endlessScroll({
+    fireOnce: true,
+    fireDelay: 500,
+    bottomPixels: 300,
+    ceaseFire: function(){
+      return $('#infinite-scroll').length ? false : true;
+    },
+    callback: function(){
+      $.ajax({
+        url: '/pics',
+        data: {
+          last: $(this).attr('last')
+        },
+        dataType: 'json',
+        success: function(data){
+          //TODO: if data is empty, then remove #infinite-scroll
+          $('li.template').loadJSON(data);
+        }
+      });
+    }
+  });
 });
