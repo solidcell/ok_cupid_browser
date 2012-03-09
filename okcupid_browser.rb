a9fbd8b105509d0da0ab88a0ec45f3c5f1d40a8c
Jesse Farless <solidcell@gmail.com>
2012-03-08 20:54:20 -0800

    infinite pagination works.  uses embeded json for the first set

--- okcupid_browser.rb
+++ okcupid_browser.rb
@@ -1,11 +1,27 @@
 class OKCBrowser < Sinatra::Base
   set :erb, :format => :html5
 
-  get "/" do
-    q = "SELECT username, url FROM pictures WHERE size = 'small' GROUP BY username LIMIT 10"
-    db = SQLite3::Database.new "db/okcupid.db"
-    @users = db.execute(q)
+  get "/pics" do
+    content_type :json
+    get_users(42, params[:last]).to_json
+  end
 
+  get "/" do
+    @users = get_users(42)
     erb :index
+  end
+
+  private
+
+  def get_users(count=42, offset=0)
+    q = "SELECT username, url
+         FROM pictures
+         WHERE size = 'small'
+         ORDER BY username
+         LIMIT #{count.to_i}
+         OFFSET #{offset.to_i}
+        "
+    db = SQLite3::Database.new "db/okcupid.db"
+    db.execute(q)
   end
 end
--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,33 @@
 $(document).ready(function() {
-  var image_paths = $("#image_paths").html();
+  // Load embedded JSON into list templates
+  image_paths = $("#image_paths").html();
   image_paths = $.parseJSON(image_paths);
-  Tempo.prepare("pictures").render(image_paths);
+  tempo = Tempo.prepare("pictures").render(image_paths);
 
+  // Set the list templates to lazy load the images
   $("img.lazy").lazyload();
+
+  // Request more JSON when scrolling close to the bottom
+  pixelBuffer = 200;
+  alreadyloading = false;
+  last = parseInt($('#pictures').attr('last'));
+  $(window).scroll(function() {
+    if ($('body').height() <= (pixelBuffer +
+                               $(window).height() +
+                               $(window).scrollTop())) {
+      if (alreadyloading == false) {
+        alreadyloading = true;
+        $.getJSON(
+          "/pics",
+          {last: last},
+          function(data) {
+            tempo.append(data);
+            $("img.lazy").lazyload();
+            last += data.length;
+            alreadyloading = false;
+          }
+        );
+      }
+    }
+  });
 });
