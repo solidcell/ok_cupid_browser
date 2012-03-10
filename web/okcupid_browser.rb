f79acf46e1759ad1e90ca36bd4c32651e7b59390
ephekt <mfrosengarten@gmail.com>
2012-03-09 20:51:37 -0800

    adding in filters

--- web/okcupid_browser.rb
+++ web/okcupid_browser.rb
@@ -3,24 +3,39 @@ class OKCBrowser < Sinatra::Base
 
   get "/pics" do
     content_type :json
-    get_users(42, params[:last]).to_json
+    get_users(42, params[:last],params[:loc]).to_json
   end
 
   get "/" do
-    @users = get_users(42)
+    db = SQLite3::Database.new "#{ROOT_PATH}/db/okcupid.db"
+    @locations = db.execute("SELECT DISTINCT location FROM profiles")
+    @locations = @locations.flatten(1).reject {|x| x.empty? }
+    @users = get_users(42,0,params[:loc])
     erb :index
   end
 
   private
 
-  def get_users(count=42, offset=0)
-    q = "SELECT username, url
+  def get_users(count=42, offset=0,location="All")
+    q = if location.nil? || location.empty? || location == "All"
+      "SELECT username, url
          FROM pictures
          WHERE size = 'small'
          ORDER BY username
          LIMIT #{count.to_i}
          OFFSET #{offset.to_i}
         "
+    else
+      location = CGI::unescape(location).gsub("'","''")
+      "SELECT pictures.username, pictures.url
+         FROM pictures
+         JOIN profiles on profiles.username = pictures.username
+         WHERE size = 'small' AND location = '#{location}'
+         ORDER BY pictures.username
+         LIMIT #{count.to_i}
+         OFFSET #{offset.to_i}
+        "
+    end
     db = SQLite3::Database.new "#{ROOT_PATH}/db/okcupid.db"
     db.execute(q)
   end
--- web/public/js/script.js
+++ web/public/js/script.js
@@ -1,4 +1,25 @@
+$.urlParam = function(name){
+    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results[1] || 0;
+}
+
 $(document).ready(function() {
+	// load embedded JSON for locations
+	var locations = $.parseJSON($('#locations').html());
+	
+	var loc_sel = $('#select_location');
+	$.each(locations,function(ind,loc) {
+		loc_sel.append("<option>"+loc+"</option>");
+	});
+	
+	if($.urlParam("loc") != 0) {
+		loc_sel.val(decodeURIComponent($.urlParam("loc")));
+	}
+	
+	loc_sel.change(function(){
+		window.location = "/?loc="+$('#select_location').val();
+	});
+	
   // Load embedded JSON into list templates
   image_paths = $("#image_paths").html();
   image_paths = $.parseJSON(image_paths);
@@ -16,7 +37,10 @@ $(document).ready(function() {
         alreadyloading = true;
         $.getJSON(
           "/pics",
-          {last: last},
+          {
+						last: last,
+						loc: $('#select_location').val()
+					},
           function(data) {
             tempo.append(data);
             last += data.length;
